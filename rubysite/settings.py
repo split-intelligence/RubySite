f995865b05b7da78a4b3dc8e5499b5d6c0a690ff
great-special <sirgreatspecial@gmail.com>
2026-07-16 17:03:17 +0100

    feat: Configure environment variables and update allowed hosts in settings

--- rubysite/settings.py
+++ rubysite/settings.py
@@ -11,10 +11,17 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -25,7 +32,12 @@ SECRET_KEY = 'django-insecure-p3#gvwx0#ct9vy@3aw&u@+fn0-ra3dweq(6#pqr(vqtft(%8w_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'http://*.onrender.com',
+    'http://127.0.0.1:8000/',
+    'http://localhost:8000/',
+    'http://rubytechlearning.com'
+]
 
 
 # Application definition
